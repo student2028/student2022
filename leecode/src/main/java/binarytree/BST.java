67a15540a516ac4769a7e85410dca88c005a9082
Xiao Hua Yao <yaoxhua@cn.ibm.com>
2022-02-03 15:02:35 +0800

    fix bug

--- leecode/src/main/java/binarytree/BST.java
+++ leecode/src/main/java/binarytree/BST.java
@@ -21,10 +21,10 @@ public class BST {
     public static void main(String[] args) {
 
         TreeNode root = TreeNode.of(1, 0, 2, 2);
-         BST bst = new BST();
+        BST bst = new BST();
         printArray(bst.getMajors(root));
 
-        TreeNode root2 = TreeNode.of(2,1);
+        TreeNode root2 = TreeNode.of(2, 1);
         bst.insertNode(root2, 3);
         root2.show();
 
@@ -32,8 +32,15 @@ public class BST {
         root2.show();
 
 
-        TreeNode root3 = bst.sortedArrayToBST(new int[]{1,2,3,4,5});
+        TreeNode root3 = bst.sortedArrayToBST(new int[]{1, 2, 3, 4, 5});
         root3.show();
+
+
+        TreeNode root4 = bst.sortedArrayToBST(new int[]{10, 5, 15, 3, 7, 0, 18});
+        int low = 7, high = 15;
+        int sum = bst.rangeSumBST(root4,low,high);
+        System.out.println(sum);
+
     }
 
     /**
@@ -79,14 +86,14 @@ public class BST {
     //insert node to bst
     public TreeNode insertNode(TreeNode root, int key) {
         //如果为空 则直接返回一个新节点
-        if(root == null) return new TreeNode(key);
+        if (root == null) return new TreeNode(key);
 
         //如果根值大于key 则去左子树找地方插入
-        if(root.val > key) {
+        if (root.val > key) {
             root.left = insertNode(root.left, key);
         }
         //如果根节点小于key 则去右子树找地方插入
-        if(root.val < key)
+        if (root.val < key)
             root.right = insertNode(root.right, key);
 
         return root;
@@ -100,25 +107,24 @@ public class BST {
      * 3. 如果节点左子树为空 则返回右子树
      * 4. 如果节点右子树为空 返回左子树
      * 5.如果左右子树都不为空 则右子树占位，左子树去右子树第一个左孩子的左孩子...下面补位即可
-     *
-     * */
+     */
     public TreeNode deleteNode(TreeNode root, int key) {
-         if(root == null) return null;
+        if (root == null) return null;
 
-         if(root.val == key) {
-              //找到该节点
-             if(root.left == null) return root.right;
-             else if(root.right == null) return root.left;
-             else {
-                 TreeNode tmp = root.right;
-                 while (tmp.left != null) tmp = tmp.left;
-                 tmp.left = root.left; //左孩子 给右子中最左面的插入
-                 root = root.right; //右孩子占位
-                 return root;
-             }
-         }
-         if(root.val > key) root.left = deleteNode(root.left, key);
-         if(root.val < key) root.right = deleteNode(root.right, key);
+        if (root.val == key) {
+            //找到该节点
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            else {
+                TreeNode tmp = root.right;
+                while (tmp.left != null) tmp = tmp.left;
+                tmp.left = root.left; //左孩子 给右子中最左面的插入
+                root = root.right; //右孩子占位
+                return root;
+            }
+        }
+        if (root.val > key) root.left = deleteNode(root.left, key);
+        if (root.val < key) root.right = deleteNode(root.right, key);
 
         return root;
     }
@@ -128,20 +134,34 @@ public class BST {
      * 首先取数组中间元素的位置，不难写出int mid = (left + right) / 2;，这么写其实有一个问题，就是数值越界，
      * 例如left和right都是最大int，这么操作就越界了，在二分法 (opens new window)中尤其需要注意！
      * 这里int mid = left + ((right - left) / 2);的写法相当于是如果数组长度为偶数，中间位置有两个元素，取靠左边的。
-     *
-     *
      */
     public TreeNode sortedArrayToBST(int[] nums) {
-        return traversal(nums, 0 , nums.length - 1);
+        return traversal(nums, 0, nums.length - 1);
     }
 
     private TreeNode traversal(int[] nums, int left, int right) {
-        if( left > right) return null;
-        int mid = left + (right-left)/2;
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
         TreeNode root = new TreeNode(nums[mid]);
-        root.left = traversal(nums, left, mid -1 );
+        root.left = traversal(nums, left, mid - 1);
         root.right = traversal(nums, mid + 1, right);
         return root;
     }
+
+//    给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+            if (root == null) return 0;
+            int sum = 0;
+            if (root.val >= low && root.val <= high) {
+                sum = root.val;
+            }
+            //优化点 可以加上剪枝
+           int  left = root.val < low ? 0 : rangeSumBST(root.left, low, high);
+           int right = root.val > high ? 0 :rangeSumBST(root.right, low, high);
+            return sum + left + right;
+     }
+
+
 
 }
--- leecode/src/main/java/binarytree/Tree2List.java
+++ leecode/src/main/java/binarytree/Tree2List.java
@@ -20,39 +20,39 @@ import java.util.Stack;
  * 输出：[1,null,2,null,3,null,4,null,5,null,6]
  * <p>
  * <p>
- *     自己写的答案很丑啊，多用了一个队列，其实不需要用的
- *     递归写法看的题解
- *
+ * 自己写的答案很丑啊，多用了一个队列，其实不需要用的
+ * 递归写法看的题解
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
  */
 public class Tree2List {
 
     public static void main(String[] args) {
-        //  TreeNode root = TreeNode.of(1, 2, 5, 3, 4, null, 6);
-        TreeNode root = TreeNode.of(0);
+        TreeNode root = TreeNode.of(1, 2, 5, 3, 4, null, 6);
+        // TreeNode root = TreeNode.of(0);
 
-       // root.listShow();
+        // root.listShow();
         flattenR(root);
         root.listShow();
     }
 
     //画画图就好理解了 自己在本本上好好画画
     private static void flattenR(TreeNode root) {
-        if(root==null) return;  //中
-        flatten(root.left);     // 左
-        flatten(root.right);    // 右
+        if (root == null) return;  //中
+        flattenR(root.left);     // 左
+        flattenR(root.right);    // 右
         TreeNode last = root.right;    //存old右
         root.right = root.left;         //左移到右
         root.left = null;               // 左置空
-        while(root.right!=null) root = root.right; // 指向右末尾
+        while (root.right != null) root = root.right; // 指向右末尾
         root.right = last;         //拼接
 
     }
 
     //即原地修改root 这种写法丑陋啊
     private static void flatten(TreeNode root) {
-        if (root == null  ) return;
+        if (root == null) return;
         // 如果只有root 也要判断  否则下面会出现环
         Stack<TreeNode> st = new Stack<>();
         st.push(root);
@@ -67,7 +67,7 @@ public class Tree2List {
         TreeNode temp = root;
         while (!qu.isEmpty()) {
             TreeNode node = qu.poll();
-            if(temp != node) {
+            if (temp != node) {
                 temp.left = null;
                 temp.right = node;
                 temp = node;
